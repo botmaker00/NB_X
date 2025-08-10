2ad74b353d21feee0b422e15f914a4401ec18bf1
S3HID N3W3B <166154843+NBOTZ001@users.noreply.github.com>
2025-08-10 11:05:53 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -216,7 +216,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/nawabbotupdate">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/nawabbotupdate">[ ᴅʀᴇᴀᴍxʙᴏᴛᴢ ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/nawabbotupdate">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/nawabbotupdate">[ NᴀᴡᴀʙxBᴏᴛs ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
