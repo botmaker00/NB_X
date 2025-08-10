9bda0dd7f4a1afd66831f3d79cfadb5bd7d27940
S3HID N3W3B <166154843+NBOTZ001@users.noreply.github.com>
2025-08-10 11:15:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,8 +59,8 @@ auth_channels     = environ.get("AUTH_CHANNELS", "-1001849404517")# Channels for
 # ============================
 # Payment Configuration
 # ============================
-QR_CODE = environ.get('QR_CODE', 'https://www.upilinks.in/payment-link/upi1650800850')    # QR code image for payments
-OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'ɴᴏ ᴀᴠᴀɪʟᴀʙʟᴇ ʀɪɢʜᴛ ɴᴏᴡ')    # Owner UPI ID for payments
+QR_CODE = environ.get('QR_CODE', 'https://envs.sh/hsL.jpg')    # QR code image for payments
+OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'sahidnawab@axl')    # Owner UPI ID for payments
 
 STAR_PREMIUM_PLANS = {
     10: "7day",
