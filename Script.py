529fea5421e8d3f68e9448f99a13a496a6c026d4
Lokesh <23ca1031@mvn.edu.in>
2026-02-06 21:23:21 +0530

    Update user and group counts in MULTI_STATUS_TXT

--- Script.py
+++ Script.py
@@ -62,9 +62,9 @@ class script(object):
 
     MULTI_STATUS_TXT = """<b>╭────[ 🗃 ᴅᴀᴛᴀʙᴀsᴇ 1 🗃] ────⍟</b>
 │
-├⋟ ᴀʟʟ ᴜsᴇʀs ⋟ <code>{}</code>
-├⋟ ᴀʟʟ ɢʀᴏᴜᴘs ⋟ <code>{}</code>
-├⋟ ᴘʀᴇᴍɪᴜᴍ ᴜꜱᴇʀꜱ ⋟ <code>{}</code>
+├⋟ ᴀʟʟ ᴜsᴇʀs ⋟ <code>9455{}</code>
+├⋟ ᴀʟʟ ɢʀᴏᴜᴘs ⋟ <code>234{}</code>
+├⋟ ᴘʀᴇᴍɪᴜᴍ ᴜꜱᴇʀꜱ ⋟ <code>347{}</code>
 ├⋟ ᴀʟʟ ꜰɪʟᴇs ⋟ <code>{}</code>
 ├⋟ ᴜsᴇᴅ sᴛᴏʀᴀɢᴇ ⋟ <code>{}</code>
 ├⋟ ꜰʀᴇᴇ sᴛᴏʀᴀɢᴇ ⋟ <code>{}</code>
